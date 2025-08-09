ee65536f56dc8be86c23de3dc07a678a207136e5
Colin Chadwick <social@chadwick.marketing>
2025-08-09 18:16:21 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly. Hallo Dieter.
+            Save and see your changes instantly. Hallo Welt.
           </li>
         </ol>
 
